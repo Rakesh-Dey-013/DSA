bc71c96549e3d972f16a2ebdd20bf828c5905c84
Rakesh-Dey-013 <rakesh.coding.007@gmail.com>
2025-09-06 12:00:40 +0530

    Refactor Linear Search function to improve readability by adding whitespace

--- Recursion/Linear_Search.cpp
+++ Recursion/Linear_Search.cpp
@@ -3,12 +3,15 @@
 using namespace std;
 
 // Linear Search using Recursion
+
 bool linearSearch(vector<int> &nums, int index, int target)
 {
     if (index == nums.size())
         return false;
+
     if (nums[index] == target)
         return true;
+        
     return linearSearch(nums, index + 1, target);
 }
 
